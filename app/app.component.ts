9784421c335bd776b7147d215c1012eed405a77c
Rob Anderson <rob.anderson2@gmail.com>
2016-11-30 22:21:00 -0800

    Change selector

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'racing-app',
+  template: `
+    <h1>{{heading}}</h1>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  heading = "Ultra Racing Schedule";
+}
+
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+//import { RacesComponent } from './races.component';
+//import { RaceService } from './race.service';
 
-import { AppModule } from './app.module';
+
+@NgModule({
+  imports: [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, 
+  //RacesComponent 
+  ],
+  //providers: [ RaceService ],
+  bootstrap: [ AppComponent ]
+})
+class AppModule {}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
